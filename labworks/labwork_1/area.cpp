061b7c95f41c69e53ecfa7e18a7254b1d03ad679
subashshakya <subashshakya12@gmail.com>
2021-06-02 13:47:46 +0545

    labworks of c++

---
+++ labworks/labwork_1/area.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    float height,base,area;
+    cout<<"enter the height of the triangle:";
+    cin>>height;
+    cout<<"enter the base of the triangle:";
+    cin>>base;
+    area=(0.5)*(base)*(height);
+    cout<<"the area of the traingle is:"<<area;
+}
---
+++ labworks/labwork_1/areanew.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+    float a,b,c,s,area;
+    cout<<"enter the first side of the triangle:";
+    cin>>a;
+    cout<<"enter the second side of the triangle:";
+    cin>>b;
+    cout<<"enter the thrid side of the triangle:";
+    cin>>c;
+    s=(a+b+c)/2;
+    area=sqrt(s*(s-a)*(s-b)*(s-c));
+    cout<<"the area of the required triangle is:"<<area;
+}
