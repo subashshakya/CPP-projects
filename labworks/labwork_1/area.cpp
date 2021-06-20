54c94aa4a69f99d07fe716a1e4eccc742a997561
subashshakya <subashshakya12@gmail.com>
2021-06-20 13:32:25 +0545

    AoT without maths library

--- labworks/labwork_1/area.cpp
+++ labworks/labwork_1/area.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main()
 {
+    //without maths library
     float height,base,area;
     cout<<"enter the height of the triangle:";
     cin>>height;
     cout<<"enter the base of the triangle:";
     cin>>base;
     area=(0.5)*(base)*(height);
-    cout<<"the area of the traingle is:"<<area;
+    cout<<"the area of the traingle is: "<<area;
 }
