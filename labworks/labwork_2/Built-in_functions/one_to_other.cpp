c0ea265bfc73677c937e2dd21443ce8ab78a7883
subashshakya <subashshakya12@gmail.com>
2021-06-20 13:54:57 +0545

    string interchange

--- labworks/labwork_2/Built-in_functions/one_to_other.cpp
+++ labworks/labwork_2/Built-in_functions/one_to_other.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    //string interchange
+    //program for string interchange
     char str1[20],str2[20];
     cout<<"\nEnter a string for interchange:";
     cin>>str1;
