df07753c568d100e47b1ef03a69db48f8b385198
subashshakya12@gmail.com <subashshakya>
2021-07-09 23:10:50 +0545

    How Single Inheritance is executed

--- OOP_projects/Types_of_Inheritance/Single_Inheritance.cc
+++ OOP_projects/Types_of_Inheritance/Single_Inheritance.cc
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+//A parent class is made
 class parent
 {
     public:
@@ -11,12 +12,13 @@ class parent
     }
 };
 
+//the CHILD class only inherits one class so this is an example of Single Inheritance
 class child:public parent
 {
     public:
     child()
     {
-        cout<<"\nThis is the child class or derived class.";
+        cout<<"\nThis is the child class or derived class."<<endl;
     }
 };
 
@@ -25,3 +27,5 @@ int main()
     child c1;
     return 0;
 }
+
+//the output will be: Firstly the parent or base class is called and then the child or derived class is called.
